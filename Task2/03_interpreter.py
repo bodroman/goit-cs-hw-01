f018bb4fe094678ec94288c91a3fbe5b599cde76
Ваше Ім'я <you@example.com>
2024-09-26 01:21:05 +0300

    added task 2

---
+++ Task2/03_interpreter.py
@@ -0,0 +1,121 @@
+class LexicalError(Exception):
+    pass
+
+
+class ParsingError(Exception):
+    pass
+
+
+class TokenType:
+    INTEGER = "INTEGER"
+    PLUS = "PLUS"
+    MINUS = "MINUS"
+    MUL = "MUL"  # Множення
+    DIV = "DIV"  # Ділення
+    LPAREN = "LPAREN"  # Відкриваюча дужка
+    RPAREN = "RPAREN"  # Закриваюча дужка
+    EOF = "EOF"  # Означає кінець вхідного рядка
+
+
+class Token:
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def __str__(self):
+        return f"Token({self.type}, {repr(self.value)})"
+
+
+class Lexer:
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.current_char = self.text[self.pos]
+
+    def advance(self):
+        """Переміщуємо 'вказівник' на наступний символ вхідного рядка"""
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None  # Означає кінець введення
+        else:
+            self.current_char = self.text[self.pos]
+
+    def skip_whitespace(self):
+        """Пропускаємо пробільні символи."""
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def integer(self):
+        """Повертаємо ціле число, зібране з послідовності цифр."""
+        result = ""
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
+
+    def get_next_token(self):
+        """Лексичний аналізатор, що розбиває вхідний рядок на токени."""
+        while self.current_char is not None:
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
+
+            if self.current_char.isdigit():
+                return Token(TokenType.INTEGER, self.integer())
+
+            if self.current_char == "+":
+                self.advance()
+                return Token(TokenType.PLUS, "+")
+
+            if self.current_char == "-":
+                self.advance()
+                return Token(TokenType.MINUS, "-")
+
+            if self.current_char == "*":
+                self.advance()
+                return Token(TokenType.MUL, "*")
+
+            if self.current_char == "/":
+                self.advance()
+                return Token(TokenType.DIV, "/")
+
+            if self.current_char == "(":
+                self.advance()
+                return Token(TokenType.LPAREN, "(")
+
+            if self.current_char == ")":
+                self.advance()
+                return Token(TokenType.RPAREN, ")")
+
+            raise LexicalError("Помилка лексичного аналізу")
+
+        return Token(TokenType.EOF, None)
+
+
+class AST:
+    pass
+
+
+class BinOp(AST):
+    def __init__(self, left, op, right):
+        self.left = left
+        self.op = op
+        self.right = right
+
+
+class Num(AST):
+    def __init__(self, token):
+        self.token = token
+        self.value = token.value
+
+
+class Parser:
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.current_token = self.lexer.get_next_token()
+
+    def error(self):
+        raise ParsingError("Помилка синтаксичного аналізу")
+
+    def eat(self, token_type):
+        """Порівнюємо поточний токен з очікуваним токеном і, якщо вони збігаються"""
